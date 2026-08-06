52ef3c2e2e3d8c54432390883fe261b9f9221c52
ataevsaid-dotcom <ataev.said@bk.ru>
2026-08-06 17:33:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -508,7 +508,7 @@ async def post_shutdown(app: Application):
 # ======================
 
 def main():
-    token = os.environ.get("BOT_TOKEN")
+    token = os.environ.get("8996509464:AAGk2ngSU8Dwi0rTohF5siAKzOhIFD_cW5k")
     if not token:
         raise SystemExit(
             "Переменная окружения BOT_TOKEN не задана. "
